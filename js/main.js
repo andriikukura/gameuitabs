823398ed85d044c4f13373d9733a152bfad084c2
andriikukura <andreikakukura@gmail.com>
2018-02-18 01:54:23 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$('.ba-card__tab').on('click', function(evt) {
+		evt.preventDefault();
+
+		$('.ba-card__tab').removeClass('active');
+		$(this).addClass('active');
+
+		$('.ba-card__main').hide();
+		$( $(this).attr('href') ).show();
+	});
+
+	
+})
